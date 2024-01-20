1dbdc9d11b3fb88b50e7673af4d0b85fc4cc4688
Aziza_Alijonova <2110062@newuu.uz>
2024-01-20 14:37:22 +0000

    kinda final

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,3 +1,10 @@
+import 'package:flutter/material.dart';
+import 'cart_page.dart';
+import 'favorites_page.dart';
+import 'user_profile.dart';
+import 'sign_in.dart';
+import 'registration_page.dart';
+
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
